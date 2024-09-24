5983ebb023481f91a5dabe2e4ea93fe65575f51a
RaquelMelow <raquelmelo949@gmail.com>
2024-09-24 20:45:24 +0100

    Perfil de usuario basico

---
+++ app/src/main/java/com/example/constraintlayout/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.constraintlayout
+
+import android.text.TextWatcher
+import android.os.Bundle
+import android.text.Editable
+import android.widget.SeekBar
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.constraintlayout.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.nameUser.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+
+        binding.seekbarAge.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.ageUser.text = "Edad: $progress"
+                binding.progressBarExperience.progress = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        binding.saveButtom.setOnClickListener{
+            Toast.makeText(this,"Datos guardados", Toast.LENGTH_SHORT).show()
+        }
+
+        enableEdgeToEdge()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
